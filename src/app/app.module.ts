72ec8e0fba1d0eaef7968301c77be05e60141284
Jorge Villalta <jorgevillaltapeyro@gmail.com>
2018-08-28 20:03:45 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [NavbarComponent],
+  imports: [BrowserModule],
+  entryComponents: [NavbarComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const appNavbar = createCustomElement(NavbarComponent, { injector });
+    customElements.define('app-navbar', appNavbar);
+  }
+
+  ngDoBootstrap() {}
+}
